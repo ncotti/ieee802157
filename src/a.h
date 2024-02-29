744b024a979d955f562f0d8f049bf7c2265b5fd0
ncotti <ncotti@frba.utn.edu.ar>
2024-02-29 17:08:32 -0300

    first commit

---
+++ src/a.h
@@ -0,0 +1,15 @@
+/*
+ * a.h
+ *
+ *  Created on: Feb 28, 2024
+ *      Author: cotti
+ */
+
+#ifndef A_H_
+#define A_H_
+
+
+
+
+
+#endif /* A_H_ */
---
+++ src/bit_macros.h
@@ -0,0 +1,16 @@
+/*
+ * bit_macros.h
+ *
+ *  Created on: Feb 29, 2024
+ *      Author: cotti
+ */
+
+#ifndef BIT_MACROS_H_
+#define BIT_MACROS_H_
+
+#define BIT_SET(reg, value, pos)   { reg |= (value << pos); }
+#define BIT_CLEAR(reg, value, pos) { reg &= ~(value << pos); }
+#define BIT_GET(reg, pos, bits)    { (reg >> pos) & bits; }
+
+
+#endif /* BIT_MACROS_H_ */
